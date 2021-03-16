178d5d3ed1944c35b7e8cd4a4148fc637c773c3b
LAPTOP-LK4VNMJ2\HP Laptop <aminnoura@gmail.com>
2021-03-16 23:51:44 +0100

    add necessary configurations for graphql

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 /**
  * @format
  */
-
+ import React from 'react';
 import {AppRegistry} from 'react-native';
-import App from './App';
+import Application from './App';
 import {name as appName} from './app.json';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+// Initialize Apollo Client
+const client = new ApolloClient({
+    uri: 'http://192.168.2.113:4000/graphql',
+    cache: new InMemoryCache()
+});
+
+const App = () => (
+    <ApolloProvider client={client}>
+        <Application />
+    </ApolloProvider>
+);
 
 AppRegistry.registerComponent(appName, () => App);
