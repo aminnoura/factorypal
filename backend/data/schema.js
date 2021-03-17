5217bc09e9d90f9fb815126477836e998938cf85
LAPTOP-LK4VNMJ2\HP Laptop <aminnoura@gmail.com>
2021-03-17 18:59:55 +0100

    BACKEND: Update data and types based on the task

--- backend/data/schema.js
+++ backend/data/schema.js
@@ -1,12 +1,30 @@
 const { makeExecutableSchema } = require("graphql-tools");
 const resolvers = require("./resolvers");
 
+// use a better naming for list based on the list
+
 const typeDefs = `
 	type Query {
-		list: [list]!
+		performanceData: [performanceDataType]!
 	}
-	type list {
-		name: String
+	type performanceDataType {
+		label: String!
+		value: Float!
+		type: VALUE_TYPE!
+		description: String!
+		category: Category!
+	}
+	enum VALUE_TYPE {
+		percentage
+		number
+		secs
+		minutes
+		hours
+	}
+	enum Category {
+		efficiency
+		shift
+		downtime
 	}
 `;
 
