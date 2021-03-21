f339da69f885d0b1fa311486141c7aeafe68dca6
LAPTOP-LK4VNMJ2\HP Laptop <aminnoura@gmail.com>
2021-03-21 12:46:54 +0100

    add click option to chartkit

--- src/components/chartkits/ChartKits.tsx
+++ src/components/chartkits/ChartKits.tsx
@@ -1,39 +1,42 @@
 import React, { FC } from 'react';
-import { SafeAreaView, ScrollView, StyleSheet, View } from 'react-native';
+import { SafeAreaView, ScrollView, StyleSheet, View, Dimensions, TouchableOpacity } from 'react-native';
 import { StackedBarChartDataType } from '../../graphql/types';
 import {StackedBarChart} from "react-native-chart-kit";
 import { colors } from '../../helpers/colors';
 
 type chartKitsPropTypes = {
 	stackedBarChartData: StackedBarChartDataType[]
+	setActiveCategory: Function
 }
 
-const ChartKits:FC<chartKitsPropTypes> = ({stackedBarChartData}) => {
+const ChartKits:FC<chartKitsPropTypes> = ({stackedBarChartData, setActiveCategory}) => {
 	const chartConfig = {
-		barPercentage: 1,
-		backgroundColor: colors.green1,
-		backgroundGradientFrom: colors.green1,
-		backgroundGradientTo: colors.green1,
+		barPercentage: 1.3,
+		backgroundColor: colors.green2,
+		backgroundGradientFrom: colors.green2,
+		backgroundGradientTo: colors.green2,
 		color: (opacity = 1) => `rgba(20, 20, 110, ${opacity})`,
 		labelColor: (opacity = 1) => `rgba(20, 20, 110, ${opacity})`
 	}
 
 	return (
 		<SafeAreaView style={styles.safeAreaViewStyle}>
-			<ScrollView contentContainerStyle={styles.scrollViewStyle} contentInsetAdjustmentBehavior="automatic" horizontal={true}>
+			<ScrollView nestedScrollEnabled = {true} contentContainerStyle={styles.scrollViewStyle} contentInsetAdjustmentBehavior="automatic" horizontal={true}>
 				<View style={styles.groupStackBarViewStyle}>
 					{stackedBarChartData && stackedBarChartData.length>0 && stackedBarChartData.map( (stackedBarData, index) =>{
 						return (
-							<StackedBarChart
-								key={index}
-								style={styles.graphStyle}
-								data={stackedBarData}
-								width={250}
-								height={320}
-								chartConfig={chartConfig}
-								withHorizontalLabels={true}
-								hideLegend={false}
-							/>
+							<TouchableOpacity onPress={()=>setActiveCategory(stackedBarData.category)}>
+								<StackedBarChart
+									key={index}
+									style={styles.graphStyle}
+									data={stackedBarData}
+									width={250}
+									height={320}
+									chartConfig={chartConfig}
+									withHorizontalLabels={true}
+									hideLegend={false}
+								/>
+							</TouchableOpacity>
 						)
 					})}
 
@@ -46,7 +49,7 @@ const ChartKits:FC<chartKitsPropTypes> = ({stackedBarChartData}) => {
 const styles = StyleSheet.create({
 	safeAreaViewStyle: {
 		display: "flex",
-		flexGrow: 1,
+		width: Dimensions.get('window').width
 	},
 	scrollViewStyle: {
 		display: "flex",
