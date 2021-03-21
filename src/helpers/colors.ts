41be34583c656e2b212d22db57eb8844dc34de24
LAPTOP-LK4VNMJ2\HP Laptop <aminnoura@gmail.com>
2021-03-21 12:45:42 +0100

    add a function in colors

--- src/helpers/colors.ts
+++ src/helpers/colors.ts
@@ -2,5 +2,9 @@ export const colors = {
     green1: "#306f30",
     green2: "#30af30",
     green3: "#30ff30",
-    blue: "#3030ff",
+    blue: "#3030ff"
+}
+
+export const stackBarColors = (index:number) => {
+    return `rgba(${index*30<250?index*30:250}, 120,130,1)`
 }
